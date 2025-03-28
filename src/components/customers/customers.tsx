a46768b679a728bcb58dbbc06e2a2c06f70e99c5
GabrielCarvalho03 <gabrielcarvalho1734@gmail.com>
2025-03-28 14:51:05 -0300

    chart in dashboard

--- src/components/customers/customers.tsx
+++ src/components/customers/customers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ShareIcon from "@/assets/icons/share-icon";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { useModalSendMessage } from "@/hook/use-modal-send-message/use-modal-send-message";
 import { useUser } from "@/hook/useUser/use-user";
@@ -11,27 +11,21 @@ import { UserPlan } from "../user-plan/user-plan";
 import { Loader } from "../Loader/loader";
 import { Filter } from "../filter/filter";
 import moment from "moment";
+import { Label, Pie, PieChart } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { userObject } from "@/hook/useUser/types";
 
-type Customer = {
-  name: string;
-  userNumber: string;
-  email: string;
-  plan: string;
-  lastPayment: string;
-  createdAt: string;
-};
+moment().locale("pt-BR");
 
 export function Customers() {
   const router = useRouter();
-  const {
-    customers,
-    setCustomers,
-    loadingGetUser,
-    handleGetUser,
-    handleOrderBy,
-  } = useUser();
+  const { customers, loadingGetUser, handleGetUser, handleOrderBy } = useUser();
   const { handleChangeUserObj } = UseUserDetails();
-  // const [customers, setCustomers] = useState<Customer[]>([]);
 
   useEffect(() => {
     if (!customers.length) {
@@ -43,12 +37,164 @@ export function Customers() {
     return <Loader />;
   }
 
+  const firstWeekStart = moment().startOf("month").startOf("week"); // Começo da primeira semana
+  const firstWeekEnd = firstWeekStart.clone().add(6, "days").endOf("day"); // Fim da primeira semana
+
+  const secondWeekStart = firstWeekEnd.clone().add(1, "days"); // Começo da segunda semana
+  const secondWeekEnd = secondWeekStart.clone().add(6, "days").endOf("day"); // Fim da segunda semana
+
+  const thirdWeekStart = secondWeekEnd.clone().add(1, "days"); // Começo da terceira semana
+  const thirdWeekEnd = thirdWeekStart.clone().add(6, "days").endOf("day"); // Fim da terceira semana
+
+  const fourthWeekStart = thirdWeekEnd.clone().add(1, "days"); // Começo da quarta semana
+  const fourthWeekEnd = fourthWeekStart.clone().add(6, "days").endOf("day"); // Fim da quarta semana
+
+  const filterUsersByWeek = (
+    customers: userObject[],
+    start: moment.Moment,
+    end: moment.Moment
+  ) => {
+    return customers.filter((customers) => {
+      const createdAt = moment(customers.createdAt);
+      return createdAt.isBetween(start, end, null, "[]"); // Verifica se está entre o intervalo
+    });
+  };
+
+  const weekUsers = [
+    filterUsersByWeek(customers, firstWeekStart, firstWeekEnd), // Primeira semana
+    filterUsersByWeek(customers, secondWeekStart, secondWeekEnd), // Segunda semana
+    filterUsersByWeek(customers, thirdWeekStart, thirdWeekEnd), // Terceira semana
+    filterUsersByWeek(customers, fourthWeekStart, fourthWeekEnd), // Quarta semana
+  ];
+
+  console.log("Quantidade de usuários na Quarta Semana:", weekUsers[3].length);
+
+  const chartData = [
+    {
+      Semana: "Semana 1",
+      usuarios: filterUsersByWeek(customers, firstWeekStart, firstWeekEnd)
+        .length,
+      fill: "#3a86ff",
+    },
+    {
+      Semana: "Semana 2",
+      usuarios: filterUsersByWeek(customers, secondWeekStart, secondWeekEnd)
+        .length,
+      fill: "#1b263b",
+    },
+    {
+      Semana: "Semana 3",
+      usuarios: filterUsersByWeek(customers, thirdWeekStart, thirdWeekEnd)
+        .length,
+      fill: "#003049",
+    },
+    {
+      Semana: "Semana 4",
+      usuarios: filterUsersByWeek(customers, fourthWeekStart, fourthWeekEnd)
+        .length,
+      fill: "#90e0ef",
+    },
+  ];
+
+  const chartConfig = {
+    Semana: {
+      label: "visitors",
+      color: "#4F46E5",
+    },
+    safari: {
+      label: "Safari",
+      color: "#ff006e",
+    },
+    firefox: {
+      label: "Firefox",
+      color: "hsl(var(--chart-3))",
+    },
+    edge: {
+      label: "Edge",
+      color: "hsl(var(--chart-4))",
+    },
+    other: {
+      label: "Other",
+      color: "hsl(var(--chart-5))",
+    },
+  } satisfies ChartConfig;
+
+  const getUsersThisMonth = (customers: userObject[]) => {
+    const currentMonth = moment().month(); // Mês atual (0-11)
+    const currentYear = moment().year(); // Ano atual
+
+    return customers.filter((customer) => {
+      const customerMonth = moment(customer.createdAt).month();
+      const customerYear = moment(customer.createdAt).year();
+      return customerMonth === currentMonth && customerYear === currentYear;
+    });
+  };
+
   return (
     <main className="w-full h-full bg-bg-primary overflow-y-auto pb-10 ">
       <div className="mt-10 pl-10">
         <h1 className="text-white text-2xl font-madefor font-bold">
           Bem-vindo ao seu <span className="text-bg-blue-ocean">dashboard</span>
         </h1>
+      </div>
+
+      <div className="min-h-32 w-full  ">
+        <ChartContainer
+          config={chartConfig}
+          className=" max-h-[250px] aspect-square mx-auto"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="usuarios"
+              nameKey="Semana"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) - 30}
+                          className="fill-white  font-bold text-white"
+                        >
+                          {moment().format("MM/YYYY")}
+                        </tspan>
+
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-white text-3xl font-bold text-white"
+                        >
+                          {getUsersThisMonth(customers).length}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          usuários
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
       </div>
 
       <div className="w-full px-10 flex justify-between items-center">
